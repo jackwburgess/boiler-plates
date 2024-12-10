cee1600947bc8abe9e5a781403fbf846d98be484
jackwburgess <57989781+jackwburgess@users.noreply.github.com>
2024-12-10 15:48:05 +0000

    Update boilerplate.tf

--- conf/boilerplate.tf
+++ conf/boilerplate.tf
@@ -1,14 +1,13 @@
 ## TEAMS DECLARATION ##### ##### ##### ##### ##### ##### ##### ##### #####
 
 variable "team_names" {
-  default = ["grosserver", "acme"]
+  default = ["team1", "team2", "team3", "team4", "team5"]
 }
 
 data "datadog_team" "teams" {
   count          = length(var.team_names)
   filter_keyword = var.team_names[count.index]
 }
-
 
 ## TEAMS RESOURCES CREATION ##### ##### ##### ##### ##### ##### ##### ##### #####
 
@@ -18,7 +17,7 @@ data "datadog_team" "teams" {
 resource "datadog_api_key" "team_keys" {
   for_each = { for idx, team in data.datadog_team.teams : idx => team }
 
-  name = "API Key for Team ${each.value.filter_keyword}"
+  name = "API Key for ${each.value.filter_keyword}"
 }
 
 
@@ -55,4 +54,32 @@ resource "datadog_service_account_application_key" "app_keys" {
     service_account_id = each.value.id
     name               = "Application key for managing ${each.value.name}"    
 
+}
+
+## LOG INDEXES
+
+# A sample Datadog logs index resource definition.
+
+resource "datadog_logs_index" "sample_index" {
+  for_each = { for idx, team in data.datadog_team.teams : idx => team }
+  name  = "${each.value.filter_keyword}-index"
+  daily_limit = 200000
+  daily_limit_reset {
+    reset_time       = "14:00"
+    reset_utc_offset = "+02:00"
+  }
+  daily_limit_warning_threshold_percentage = 50
+  retention_days                           = 15
+  flex_retention_days                      = 180
+  filter {
+    query = "team:${each.value.handle}"
+  }
+  exclusion_filter {
+    name       = "debug logs"
+    is_enabled = true
+    filter {
+      query       = "status:debug"
+      sample_rate = 1.0
+    }
+  }
 }
